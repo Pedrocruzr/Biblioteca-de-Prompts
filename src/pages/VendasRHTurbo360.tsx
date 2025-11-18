9a54bb3b449e687b3532dcdf5585c237cb7651e1
[dyad] <pedrocruzr@gmail.com>
2025-11-18 07:27:36 -0300

    [dyad] Correcting the hero section logo - wrote 1 file(s), deleted 1 file(s)

--- src/pages/VendasRHTurbo360.tsx
+++ src/pages/VendasRHTurbo360.tsx
@@ -10,8 +10,6 @@ import { TestimonialCard } from "@/components/VendasRH/TestimonialCard";
 import heroImage from "@/assets/hero-businesswoman.png";
 import pedroNetoImage from "@/assets/pedro-neto.png";
 import rhTurboLogo from "@/assets/rh-turbo-360-logo.png";
-import rhTurboHeaderLogo from "@/assets/rh-turbo-header-logo.png";
-import rhTurboHeroLogoNew from "@/assets/rh-turbo-hero-logo-new.svg";
 import turboBadge from "@/assets/turbo-badge.png";
 import depoimentoChinita from "@/assets/depoimento-cintia-hq.jpeg";
 import depoimentoAnneBeatriz from "@/assets/depoimento-anne-hq.jpeg";
@@ -69,7 +67,7 @@ const VendasRHTurbo360 = () => {
         <div className="flex flex-col items-center text-center max-w-4xl mx-auto animate-fade-in">
           {/* Logo */}
           <div className="mb-8">
-            <img src={rhTurboHeroLogoNew} alt="RH Turbo 360 Logo" className="w-full max-w-[266px] md:max-w-[310px] h-auto" />
+            <img src={rhTurboLogo} alt="RH Turbo 360 Logo" className="w-full max-w-[266px] md:max-w-[310px] h-auto" />
           </div>
           
           {/* Headline */}
